cedf6c2717d8d4375bd8aa0647a5bb72ccee21d6
ChrisB <chris.barnard@btinternet.com>
2018-10-09 16:07:08 +0100

    Initial commit

---
+++ PDF2IMGTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF2IMGTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pdf_filename = BarnardTech.Pdf2Image.GetProgramFilePath("pdf-sample.pdf");
+            string png_filename = BarnardTech.Pdf2Image.GetProgramFilePath("converted.png");
+
+            if (!File.Exists(pdf_filename))
+            {
+                File.WriteAllBytes(pdf_filename, Properties.Resources.pdf_sample);
+            }
+
+            if (args.Length > 2)
+            {
+                pdf_filename = args[1];
+                png_filename = args[2];
+            }
+
+            List<string> errors = BarnardTech.Pdf2Image.Convert(pdf_filename, png_filename, BarnardTech.Pdf2Image.OutputFormat.png16m);
+            if (errors.Any())
+            {
+                foreach (string error in errors)
+                {
+                    Console.WriteLine(error);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Conversion is successful.");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PDF2IMG.NET
 PDF to PNG Converter Library for .Net, using GhostScript
+
+This library is based on the [PDF2PNG](https://github.com/chen0040/cs-pdf-to-image) library by [@chen0040](https://github.com/chen0040).
+
+I've made some very minor improvements for my own usage, and wanted to put these changes up on NuGet, so I created a new repo for it.
